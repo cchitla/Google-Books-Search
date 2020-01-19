714649274d1d15f25f0f7328ad436ea805e21639
Chris Chitla <chrischitla@gmail.com>
2020-01-19 10:55:58 -0700

    cleaned up template app stuff, added basic set up of components

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import NavBar from './components/NavBar/NavBar';
+import Header from './components/Header/Header';
+import ListContainer from './components/ListContainer/ListContainer';
+import Book from './components/Book/Book';
+import Search from './components/Search/Search';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <NavBar />
+          <Header />
+          <ListContainer />
+          <Book />
+          <Search />
+          
       </div>
     );
   }
