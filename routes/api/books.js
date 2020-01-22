754d456f4d20b742932ccfd533b166902ff00dd6
Chris Chitla <chrischitla@gmail.com>
2020-01-21 21:38:28 -0700

    added api routes

---
+++ routes/api/books.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const booksController = require("../../controllers/bookController");
+
+// /api/books
+router.route("/").get(booksController.findAll).post(booksController.create);
+
+// /api/books/:id
+router.route("/:id").get(booksController.findById).put(booksController.update).delete(booksController.remove);
+
+module.exports = router;
